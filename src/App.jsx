a3dbd3cb268cf8497d0efcd8e34d95ca57e68b36
Yavuz Yalçınkaya <recepyavuzyalcinkaya@gmail.com>
2023-08-21 18:28:06 +0300

    completed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import Navbar from "./components/Navbar";
+
+const App = () => (
+  <main className="relative">
+    <Navbar />
+  </main>
+);
+
+export default App;
