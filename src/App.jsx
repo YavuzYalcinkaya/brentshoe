28c9a8b52a6be971ed66e3bfb2dae72335f88afb
Yavuz Yalçınkaya <recepyavuzyalcinkaya@gmail.com>
2023-09-12 16:08:24 +0300

    completed Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import Navbar from "./components/Navbar";
-
+import Hero from "./sections/Hero";
 const App = () => (
   <main className="relative">
     <Navbar />
+    <section className="xl:padding-l wide:padding-r padding-b">
+      <Hero />
+    </section>
   </main>
 );
 
