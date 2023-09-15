d72428fd861de5a4c682afe1abfba195181fbdee
Yavuz Yalçınkaya <recepyavuzyalcinkaya@gmail.com>
2023-09-15 14:19:10 +0300

    completed popular products

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Navbar from "./components/Navbar";
 import Hero from "./sections/Hero";
+import PopularProducts from "./sections/PopularProducts";
 const App = () => (
   <main className="relative">
     <Navbar />
     <section className="xl:padding-l wide:padding-r padding-b">
       <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
     </section>
   </main>
 );
