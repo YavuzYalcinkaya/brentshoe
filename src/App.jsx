85dbbf8fffca91abeeafd7d5c55a696b854668ed
Yavuz Yalçınkaya <recepyavuzyalcinkaya@gmail.com>
2023-09-15 16:37:44 +0300

    service and special offer completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Navbar from "./components/Navbar";
 import Hero from "./sections/Hero";
 import PopularProducts from "./sections/PopularProducts";
 import SuperQuality from "./sections/SuperQuality";
+import Services from "./sections/Services";
+import SpecialOffer from "./sections/SpecialOffer";
 const App = () => (
   <main className="relative">
     <Navbar />
@@ -13,6 +15,12 @@ const App = () => (
     </section>
     <section className="padding">
       <SuperQuality />
+    </section>
+    <section className="padding">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
     </section>
   </main>
 );
--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -1,13 +1,13 @@
 import { star } from "../assets/icons";
 
-const PopularProductCard = ({ imgURL, name, price, point }) => {
+const PopularProductCard = ({ imgURL, name, price, rating }) => {
   return (
     <div className="flex flex-1 flex-col w-full max-sm:w-full">
       <img src={imgURL} alt={name} className="w-[282px] h-[282px]" />
       <div className="mt-8 flex justify-start gap-2.5">
         <img src={star} alt="rating icon" width={24} height={24} color="red" />
         <p className="font-montserrat text-xl leading-normal text-slate-gray">
-          {point}
+          {rating}
         </p>
       </div>
       <h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin">
